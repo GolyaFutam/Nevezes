2060af1244a0dc98a0524c958798cd1f47362535
GolyaFutam <74051243+GolyaFutam@users.noreply.github.com>
2020-11-06 12:27:54 +0100

    Add files via upload

---
+++ clue.c
@@ -0,0 +1,29 @@
+#define BUT ;
+    #define SO ;
+#define SEMMI (int)NULL
+        #define SZAM 0xD
+#define kdGd_ return
+    #define theEND )
+    #include <stdio.h>
+    #define PONT '.'
+#define DONNO "%s"
+#define h6_d [
+    #define KISAUTO char
+int main( theEND {
+KISAUTO Hdu__d[0xF] BUT
+
+#define BLA [
+Hdu__d BLA SZAM] = 'i' SO SO Hdu__d BLA 0xE - 0x0] = SEMMI;
+    Hdu__d[2] = 't';
+        #include <stdbool.h>
+3 h6_d Hdu__d] = PONT SO Hdu__d[5] = 121 BUT Hdu__d BLA 6] = '/' SO
+        Hdu__d[1] = 'i' SO if(!(true) || ((true) && !(!(1))) && false);
+{
+    Hdu__d[7] = 062 BUT Hdu__d BLA 010] = 'U';
+}
+    #define KILENC '0'
+    Hdu__d BLA 2 * (7 - 2 theEND ] = 8 + KILENC BUT Hdu__d[0xB] = 'G' SO Hdu__d h6_d 014] = 'J'; SO
+Hdu__d h6_d 4 + 4 + 4 - 4 + 4 - 4 - 4] = 'l' SO
+        #define $dolla (
+    Hdu__d[0x9] = '0';;  ;;; ; Hdu__d[SEMMI] = 'b' BUT Hdu__d h6_d 0] = 0 BUT 
+printf $dolla DONNO , Hdu__d) BUT kdGd_ SEMMI; }
